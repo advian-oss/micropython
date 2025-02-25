93cf578e76e8e28aa4c83a9cb43e99ebb02b4b54
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-02-25 16:05:01 +0200

    first attempt at making a board config for WROVER_N16R8

--- ports/esp32/boards/WROVER_N16R8/mpconfigboard.cmake
+++ ports/esp32/boards/WROVER_N16R8/mpconfigboard.cmake
@@ -3,3 +3,18 @@ set(SDKCONFIG_DEFAULTS
     ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     boards/sdkconfig.ble
 )
+
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/sdkconfig.spiram
+)
+
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/WROVER_N16R8/sdkconfig.flash_16m
+)
+
+list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_BOARD_NAME="WROVER 16MB Flash and SPIRAM"
+)
+
